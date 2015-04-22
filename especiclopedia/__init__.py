5d43f54e0f75ad03cba3fd9db552138fe0517ee8
diegogeovani <diegogeovani@aol.com>
2015-04-22 18:04:19 -0300

    view p/ retorno versao da base de dados singleplayer

--- especiclopedia/__init__.py
+++ especiclopedia/__init__.py
@@ -17,6 +17,6 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('check_version', '/singleplayer/check_version')
+    config.add_route('get_version', '/singleplayer/version')
     config.scan()
     return config.make_wsgi_app()
--- especiclopedia/singleplayer/views.py
+++ especiclopedia/singleplayer/views.py
@@ -1,11 +1,23 @@
 from pyramid.response import Response
 from pyramid.view import view_config, view_defaults
 
-class VersionCheckerView:
+from .models import DatabaseVersion, DBSession
+
+from collections import OrderedDict
+
+
+class VersionView:
 
     def __init__(self, request):
         self.request = request
 
-    @view_config(route_name='check_version')
-    def check_version_view(request):
-        return Response('<h1>200 HTTP Status')
+    @view_config(route_name='get_version', renderer='json')
+    def version_view(request):
+        version = DBSession.query(DatabaseVersion).get(1)
+
+        json = OrderedDict([
+            ('mode', 'singleplayer'),
+            ('version', version.number),
+        ])
+
+        return json
